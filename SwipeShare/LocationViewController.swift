3518327c4b0a72cf6d81027e30bace0ff0027a75
aleiua <alexandra.m.lynn.16@dartmouth.edu>
2016-01-24 16:56:19 -0500

    hi

---
+++ SwipeShare/LocationViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LocationViewController.swift
+//  SwipeShare
+//
+//  Created by A. Lynn on 1/24/16.
+//  Copyright © 2016 yaw. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+
+class LocationViewController: ViewController, CLLocationManagerDelegate {
+
+    
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.startUpdatingLocation()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    func locationViewController(manager: CLLocationManager!,   didUpdateLocations locations: [AnyObject]!) {
+        mapView.showsUserLocation = (status == .AuthorizedAlways)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
