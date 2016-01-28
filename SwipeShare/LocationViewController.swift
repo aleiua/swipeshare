50351851c4abae3d5634ef64874d7e35072425a7
Troy Palmer <tbaileypalmer@gmail.com>
2016-01-27 19:16:11 -0500

    herp

--- SwipeShare/LocationViewController.swift
+++ SwipeShare/LocationViewController.swift
@@ -18,27 +18,97 @@ class LocationViewController: ViewController, CLLocationManagerDelegate {
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var imageG: UIImageView!
     
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
+    var currentHeading: CLHeading!
+    
+    
+    
+
+    var firstX:Double = 0;
+    var firstY:Double = 0;
+    
     
     
     @IBAction func getCurrentLocation(sender: AnyObject) {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        
+        
     }
+    
+    
+    func initializeGestureRecognizer()
+    {
+        //For PanGesture Recoginzation
+        let panGesture: UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("recognizePanGesture:"))
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 1
+        imageG.addGestureRecognizer(panGesture)
+    }
+    
+    /*
+    * Performs updating on objects to which the gesture recognizer is added
+    *
+    */
+    func recognizePanGesture(sender: UIPanGestureRecognizer)
+    {
+        let translate = sender.translationInView(self.view)
+        sender.view!.center = CGPoint(x:sender.view!.center.x + translate.x,
+            y:sender.view!.center.y + translate.y)
+        sender.setTranslation(CGPointZero, inView: self.view)
+        
+        // need to store first and last locations of swipe and calculate angle relative to top of screen
+        
+        
+        // Mess with this to make deceleration look natural
+//        if sender.state == UIGestureRecognizerState.Ended {
+//            // 1
+//            let velocity = sender.velocityInView(self.view)
+//            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+//            let slideMultiplier = magnitude / 200
+//            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+//            
+//            // 2
+//            let slideFactor = 0.1 * slideMultiplier     //Increase for more of a slide
+//            // 3
+//            var finalPoint = CGPoint(x:sender.view!.center.x + (velocity.x * slideFactor),
+//                y:sender.view!.center.y + (velocity.y * slideFactor))
+//            // 4
+//            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+//            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+//            
+//            // 5
+//            UIView.animateWithDuration(Double(slideFactor * 2),
+//                delay: 0,
+//                // 6
+//                options: UIViewAnimationOptions.CurveEaseOut,
+//                animations: {sender.view!.center = finalPoint },
+//                completion: nil)
+//        }
+    }
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
         locationManager.requestAlwaysAuthorization()
+        self.initializeGestureRecognizer()
+        
 
 
         // Do any additional setup after loading the view.
         
     }
+    
+
     
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
@@ -55,12 +125,22 @@ class LocationViewController: ViewController, CLLocationManagerDelegate {
         currentLocation = locationManager.location!
         latitudeLabel.text = "\(currentLocation.coordinate.latitude)"
         longitudeLabel.text = "\(currentLocation.coordinate.longitude)"
-        print("\(currentLocation.coordinate.latitude)")
+//        print("\(currentLocation.coordinate.latitude)")
 
-        print("\(currentLocation.coordinate.longitude)")
+//        print("\(currentLocation.coordinate.longitude)")
 
 
     }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        
+        currentHeading = locationManager.heading!
+        headingLabel.text = "\(currentHeading.trueHeading)"
+
+//        print("\(currentHeading.trueHeading)")
+    }
+    
 
 
 
@@ -70,6 +150,7 @@ class LocationViewController: ViewController, CLLocationManagerDelegate {
     }
 
 
+    
     /*
     // MARK: - Navigation
 
