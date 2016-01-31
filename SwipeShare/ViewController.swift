cfc781521b2ad720cc137286a9a2656fc3b1eb2d
Garrett Watumull <garrett.s.watumull@dartmouth.edu>
2016-01-31 18:22:53 -0500

    Error handling for user entry.

--- SwipeShare/ViewController.swift
+++ SwipeShare/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var password: UITextField!
     
+    let alert = UIAlertController()
+    
     
     @IBAction func submit(sender: AnyObject) {
         if (username.text == "" || password.text == "")
@@ -26,13 +28,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let user = PFUser()
         user.username = username.text
         user.password = password.text
-    
         
+
         user.signUpInBackgroundWithBlock {
             (succeeded: Bool, error: NSError?) -> Void in
+            
             if let error = error {
-                // Show the errorString somewhere and let the user try again.
-                print(error.userInfo.description)
+                // Display an alert view to show the error message
+                self.alert.title = error.userInfo.debugDescription
+                let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                self.alert.addAction(defaultAction)
+                
+                
+                self.presentViewController(self.alert, animated: true, completion: nil)
+                
+                // Bring the keyboard back up, because they probably need to change something.
+                self.username.becomeFirstResponder()
+                self.username.text = "";
+                self.password.text = "";
+                return;
 
             } else {
                 self.performSegueWithIdentifier("infoSubmitted", sender: nil)
@@ -41,6 +55,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         print("User submitted password")
     
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,8 +69,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func dismissKeyboard() {
         view.endEditing(true)
     }
-    
-
 
 
     override func didReceiveMemoryWarning() {
