b7d523d097eb3cab998d71f7a590a4515b737f90
Garrett Watumull <garrett.s.watumull@dartmouth.edu>
2016-01-31 15:59:13 -0500

    Initial Entry

--- SwipeShare/ViewController.swift
+++ SwipeShare/ViewController.swift
@@ -11,9 +11,36 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
     @IBAction func submit(sender: AnyObject) {
+        if (username.text == "" || password.text == "")
+        {
+            print("NOTHING ENTERED")
+            return
+        }
+
+        let user = PFUser()
+        user.username = username.text
+        user.password = password.text
+        
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                // Show the errorString somewhere and let the user try again.
+                print(error.userInfo.description)
+
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
         print("User submitted password")
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
