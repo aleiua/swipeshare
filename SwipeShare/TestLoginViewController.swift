b44d11ca89ddb7a0bd005c38a37cb14839644d39
Troy Palmer <tbaileypalmer@gmail.com>
2016-02-07 16:24:59 -0500

    test login interface created (drag entry point arrow to change)

---
+++ SwipeShare/TestLoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TestLoginViewController.swift
+//  SwipeShare
+//
+//  Created by Troy Palmer on 2/7/16.
+//  Copyright © 2016 yaw. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+
+class TestLoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        let username = self.usernameField.text
+        let password = self.passwordField.text
+        
+        if (username == "" || password == "")
+        {
+            print("NOTHING ENTERED")
+            return
+        }
+        
+        PFUser.logInWithUsernameInBackground(username!, password: password!, block: { (user, error) -> Void in
+            
+            if (user != nil) {
+                print("successful login")
+            } else {
+                print("login error")
+            }
+            
+        })
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
