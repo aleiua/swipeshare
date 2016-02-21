2225a48ccb5094897da30b64f105ee7e1da3213a
Robbie <rnhaus@gmail.com>
2016-02-21 16:17:49 -0500

    initial commit (coverflow still in separate app)

---
+++ SwipeShare/Yaw-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Yaw-Bridging-Header.h
+//  SwipeShare
+//
+//  Created by Robbie Neuhaus on 2/20/16.
+//  Copyright © 2016 yaw. All rights reserved.
+//
+
+//#ifndef Yaw_Bridging_Header_h
+//#define Yaw_Bridging_Header_h
+//
+//
+//#endif /* Yaw_Bridging_Header_h */
+
+#import "iCarousel.h"
+
