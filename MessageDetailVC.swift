4b327ebf09bd1a24a4c1f20a30aa902064e83f18
aleiua <lexielynn1@gmail.com>
2016-03-06 18:02:35 -0500

    Sign up still crashes - messageDetail is back

---
+++ MessageDetailVC.swift
@@ -0,0 +1,52 @@
+//
+//  MessageDetailVC.swift
+//  SwipeShare
+//
+//  Created by A. Lynn on 3/6/16.
+//  Copyright © 2016 yaw. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class MessageDetailVC: UIViewController{
+    
+    
+    var message: Message!
+    
+    
+    
+    @IBOutlet weak var messageSenderLabel: UILabel!
+    @IBOutlet weak var messageImageView: UIImageView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        messageSenderLabel.text = String(message.sender["username"])
+        messageImageView?.image = message.image
+    }
+    
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        
+        if segue.identifier == "sendAgainSegue" {
+            
+            let destinationViewController = segue.destinationViewController as! LocationViewController
+            
+            destinationViewController.image = messageImageView
+            
+            
+        }
+    }
+    
+}
--- SwipeShare/SignUpViewCont.swift
+++ SwipeShare/SignUpViewCont.swift
@@ -66,6 +66,7 @@ class SignUpViewCont: UIViewController {
                
 
 >>>>>>> FixingSignUp-fromMessageTableView
+
         })
         
         
