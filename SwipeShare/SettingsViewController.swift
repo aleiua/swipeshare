cb97a24152abdef01969f7d1f4b460bfe540ad54
Garrett Watumull <garrett.s.watumull@dartmouth.edu>
2016-03-31 20:13:59 -0400

    Logout works #dab

--- SwipeShare/SettingsViewController.swift
+++ SwipeShare/SettingsViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Parse
 
 
 class SettingsViewController: UITableViewController {
@@ -23,6 +24,16 @@ class SettingsViewController: UITableViewController {
         userIcon.layer.cornerRadius = userIcon.frame.height/2
         userIcon.clipsToBounds = true
         
+    }
+    
+    @IBAction func logout() {
+        print(PFUser.currentUser())
+        PFUser.logOut()
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login") as! LoginViewController
+            self.presentViewController(viewController, animated: true, completion: nil)
+        })
     }
 
     override func didReceiveMemoryWarning() {
