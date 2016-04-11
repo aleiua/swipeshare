28e7027a270859cad167f90659a0c6fa3ba0efc0
Garrett Watumull <garrett.s.watumull@dartmouth.edu>
2016-04-10 22:05:39 -0400

    New Controller. FB log in working.

--- SwipeShare/ViewController.swift
+++ SwipeShare/ViewController.swift
@@ -1,39 +1,89 @@
 //
-//  SignupViewController.swift
+//  LoginViewController.swift
 //  SwipeShare
 //
-//  Created by A. Lynn on 1/24/16.
+//  Created by A. Lynn on 2/3/16.
 //  Copyright © 2016 yaw. All rights reserved.
 //
 
 import UIKit
 import Parse
+import ParseUI
+import LocationKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+import FBSDKCoreKit
+import ParseFacebookUtilsV4
 
-    @IBAction func MapButton(sender: AnyObject) {
-    }
 
+class ViewController: UIViewController, UITableViewDelegate, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        for family: String in UIFont.familyNames()
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNamesForFamilyName(family)
+            {
+                print("== \(names)")
+            }
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
         
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if (PFUser.currentUser() == nil) {
+            let loginViewController = LoginViewController()
+            loginViewController.delegate = self
+            
+            loginViewController.fields = [.UsernameAndPassword, .LogInButton, .PasswordForgotten, .SignUpButton, .Facebook]
+            loginViewController.signUpController?.delegate = self
+            
+            self.presentViewController(loginViewController, animated: false, completion: nil)
+        }
+            
+        else {
+            presentLoggedInAlert()
+        }
+    }
+    
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        presentLoggedInAlert()
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        presentLoggedInAlert()
+    }
+    
+    
+    
+    func presentLoggedInAlert() {
+        let alertController = UIAlertController(title: "You're logged in", message: "Welcome to Yaw", preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alertController.addAction(OKAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
     func dismissKeyboard() {
         view.endEditing(true)
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
-
