b60838c6e64c176537a7c9e41370e45f6a719f52
Robbie <rnhaus@gmail.com>
2016-04-27 12:18:45 -0400

    friend prompt works

--- SwipeShare/FriendPromptViewController.swift
+++ SwipeShare/FriendPromptViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class FriendPromptViewController: UITableViewController {
+class FriendPromptViewController: UIViewController {
     var delegate: LocationViewController? = nil
     
     @IBOutlet weak var blurredBackgroundView: UIVisualEffectView!
@@ -23,13 +23,9 @@ class FriendPromptViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setEditing(true, animated: true)
-        tableView.backgroundView = blurredBackgroundView
+//        self.backgroundView = blurredBackgroundView
         navBar.setBackgroundImage(UIImage(), forBarMetrics:UIBarMetrics.Default)
-        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0), animated: true, scrollPosition: UITableViewScrollPosition.Middle)
-        tableView.separatorEffect = UIVibrancyEffect(forBlurEffect: blurredBackgroundView.effect as! UIBlurEffect)
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+//        tableView.separatorEffect = UIVibrancyEffect(forBlurEffect: blurredBackgroundView.effect as! UIBlurEffect)
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
@@ -39,78 +35,6 @@ class FriendPromptViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // MARK: - Table view data source
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return items.count
-//    }
-    
-    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("checkCell", forIndexPath: indexPath)
-//        
-//        // Configure the cell...
-//        cell.textLabel!.text = items[indexPath.row]["name"] as? String
-//        let bgColorView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
-//        cell.selectedBackgroundView = bgColorView
-//        //
-//        //        if(indexPath.row == 0){
-//        //            self.tableView.selectRowAtIndexPath(indexPath, animated: true, scrollPosition: UITableViewScrollPosition.Middle)
-//        //        }
-//        
-//        //        cell.textLabel!.text = String(indexPath.row)
-//        
-//        
-//        return cell
-//    }
-//    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
-//        return UITableViewCellEditingStyle(rawValue: 3)!
-//    }
-//    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-//        
-//    }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-    }
-    */
-    
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the item to be re-orderable.
-    return true
-    }
-    */
     
     /*
     // MARK: - Navigation
