afc3ed3bf98eda51a5bd901d2137608e2d745545
Garrett Watumull <garrett.s.watumull@dartmouth.edu>
2016-05-09 11:27:03 -0400

    Added new segues.

--- SwipeShare/CustomSegues.swift
+++ SwipeShare/CustomSegues.swift
@@ -56,3 +56,52 @@ class UIStoryboardUnwindSegueFromLeft: UIStoryboardSegue {
         )
     }
 }
+
+class UISegueFromLeft: UIStoryboardSegue {
+    
+    override func perform()
+    {
+        let src = self.sourceViewController as UIViewController
+        let dst = self.destinationViewController as UIViewController
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransformMakeTranslation(src.view.frame.size.width, 0)
+        
+        UIView.animateWithDuration(0.40,
+            delay: 0.0,
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {
+                dst.view.transform = CGAffineTransformMakeTranslation(0, 0)
+            },
+            completion: { finished in
+                src.presentViewController(dst, animated: false, completion: nil)
+            }
+        )
+    }
+}
+
+
+class UIStoryboardUnwindSegueFromRight: UIStoryboardSegue {
+    
+    override func perform()
+    {
+        let src = self.sourceViewController as UIViewController
+        let dst = self.destinationViewController as UIViewController
+        
+        src.view.superview?.insertSubview(dst.view, belowSubview: src.view)
+        src.view.transform = CGAffineTransformMakeTranslation(0, 0)
+        
+        UIView.animateWithDuration(0.40,
+            delay: 0.0,
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {
+                src.view.transform = CGAffineTransformMakeTranslation(src.view.frame.size.width, 0)
+            },
+            completion: { finished in
+                src.dismissViewControllerAnimated(false, completion: nil)
+            }
+        )
+    }
+}
+
+
